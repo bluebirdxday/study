c3c9c0e8c7d617b86ac8bdb005ecb6dce8a550ff
DESKTOP-IJ8C807\user1 <bluebirdxday@naver.com>
2023-02-08 12:40:00 +0900

    20230208_연습

--- 01_Java/a002_operator/src/edu/kh/operator/ex/OperatorEx1.java
+++ 01_Java/a002_operator/src/edu/kh/operator/ex/OperatorEx1.java
@@ -8,6 +8,20 @@ public class OperatorEx1 {
 		
 		Scanner scan = new Scanner(System.in);
 		
+		// 나머지 연산자 % : modulom (=mod)
+		
+		System.out.println("두 정수를 입력 받아 산술 연산 결과 출력하기");
+		
+		System.out.print("정수 입력 1 : ");
+		int input1 = scan.nextInt();
+		
+		System.out.print("정수 입력 2 : ");
+		int input2 = scan.nextInt();
+		
+		System.out.printf("%d + %d = %d \n", input1, input2, input1+input2);
+		
+		System.out.printf("%d %% %d = %d \n", input1, input2, input1%input2);
+		// printf에서 '%' 연산자를 출력하고 싶으면 "%%" 이렇게 작성
 	}
 
 }
