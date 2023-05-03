ec1347a474058f65c776839caaf50868f3ccdf2f
user1 <user1>
2023-05-03 14:04:39 +0900

    20230503_Spring_Ajax비동기통신_이메일인증

--- 06_Framework/boardProject/src/main/java/edu/kh/project/member/model/dao/AjaxDAO.java
+++ 06_Framework/boardProject/src/main/java/edu/kh/project/member/model/dao/AjaxDAO.java
@@ -1,8 +1,12 @@
 package edu.kh.project.member.model.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import edu.kh.project.member.model.dto.Member;
 
 @Repository	// DB연결 의미 + bean 등록 (IOC)
 public class AjaxDAO {
@@ -29,6 +33,16 @@ public class AjaxDAO {
 	// 닉네임 중복 검사
 	public int checkNickName(String nickname) {
 		return sqlSession.selectOne("ajaxMapper.checkNickName", nickname);
+	}
+
+	// 이메일로 회원 정보 조회
+	public Member selectMember(String email) {
+		return sqlSession.selectOne("ajaxMapper.selectMember", email);
+	}
+
+	// 이메일이 일부라도 일치하는 모든회원 조회
+	public List<Member> selectMemberList(String input) {
+		return sqlSession.selectList("ajaxMapper.selectMemberList", input);
 	}
 
 }
